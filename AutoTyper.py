c55bffc6b50e341991a37afd8281e9cfabaafd1c
AK (Aadhi Kumar) <42910742+ak-aadhi-kumar@users.noreply.github.com>
2023-10-06 17:28:18 -0400

    Update AutoTyper.py

--- AutoTyper.py
+++ AutoTyper.py
@@ -1,6 +1,4 @@
-import pyautogui
-import pyperclip
-import time
+import pyautogui, pyperclip, time
 
 time.sleep(3) # 3 = Time between executing the AutoTyper command and when it starts typing
 pyautogui.typewrite(pyperclip.paste(), 0.025) # 0.025 = delay between each keystroke in seconds, too short and it won't type it out correctly, too long and it'll take forever to finish typing
